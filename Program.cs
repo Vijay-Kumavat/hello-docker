de0c9096d20c34bb3d6569f98f5e8c08c8fd908c
Vijaykumar Kumavat <KumavatVijaykumar@JohnDeere.com>
2026-08-31 17:51:39 +0530

    Use the alpine container image

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
+using System.Runtime.InteropServices;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello DOCKER!");
+app.MapGet("/", () => $"Hello from {RuntimeInformation.OSDescription} on {RuntimeInformation.OSArchitecture}");
 
 app.Run();
